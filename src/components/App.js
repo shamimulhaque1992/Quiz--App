92203d35c117b6ea25f13cdc52578e7e3146247f
shamimulhaque1992 <khandoker15-1992@diu.edu.bd>
2021-10-06 19:25:18 +0600

    branch-8 source code push

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,16 @@ import Layout from "./Layout";
 // import Home from "./pages/Home";
 // import Signup from "./pages/Signup";
 // import Login from "./pages/Login";
-import Quiz from "./pages/Quiz";
+// import Quiz from "./pages/Quiz";
+import Result from "./pages/Result";
 function App() {
   return (
     <Layout>
       {/* <Home /> */}
       {/* <Signup /> */}
       {/* <Login /> */}
-      <Quiz />
+      {/* <Quiz /> */}
+      <Result />
     </Layout>
   );
 }
---
+++ src/components/pages/Result.js
@@ -0,0 +1,12 @@
+import Summary from "../Summary";
+import Analysis from "../Analysis";
+export default function Result() {
+    return (
+        <>
+
+        <Summary />
+        <Analysis />
+
+        </>
+    );
+}
