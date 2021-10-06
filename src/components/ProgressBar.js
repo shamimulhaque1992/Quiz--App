2f566f47878c94f0a1dad869941d9f1a6ce2c37e
shamimulhaque1992 <khandoker15-1992@diu.edu.bd>
2021-10-07 00:48:59 +0600

    branch-9 source code push

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import classes from "../styles/ProgressBar.module.css";
 import Button from "./Button";
 export default function ProgressBar() {
@@ -12,13 +13,13 @@ export default function ProgressBar() {
           <div className={classes.progress} style={{ width: "20%" }}></div>
         </div>
       </div>
-      <a href="result.html">
+      <Link to="/result">
         <Button ClassName={classes.next}>
           <span>Next Question</span>
           <span className="material-icons-outlined"> arrow_forward </span>
         </Button>
 
-      </a>
+      </Link>
     </div>
 
   );
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,4 +1,4 @@
-import Video from "../Videos"
+import Videos from "../Videos"
 export default function Home() {
-    return <Video />;
+    return <Videos />;
 }
